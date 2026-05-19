092f46d67e60c0ecb8d2e625d3de2530e9ae4b5a
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 15:23:06 +0700

    implement RekeningBank constructor with manual string copy

--- practicum.cpp
+++ practicum.cpp
@@ -6,4 +6,18 @@ using namespace std;
 protected:
     char namaNasabah[50];
     double saldo;
-int main(){}
+public:
+    RekeningBank(const char nama[], double saldoAwal)
+    {
+        int i = 0;
+
+        // Copy character array manually
+        while (nama[i] != '\0')
+        {
+            namaNasabah[i] = nama[i];
+            i++;
+        }
+
+        namaNasabah[i] = '\0';
+        saldo = saldoAwal;
+    }int main(){}
