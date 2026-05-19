bdac914f9a6ed6ac809fc431c242214e73ab8430
Hussain512 <shujaatfida512@gmail.com>
2026-05-19 11:05:40 +0700

    writing objects in main

---
+++ encapsulation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu
+{
+private:
+    string saklarNo[10];
+
+public:
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+
+    string getSaklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
+
+int main()
+{
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    
+}
